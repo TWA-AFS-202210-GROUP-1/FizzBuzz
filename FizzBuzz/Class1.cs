0accedd2714041e90833622fcecdf4ff33d4b429
Frank <zongpengchen@gmail.com>
2022-10-26 21:56:25 +0800

    done the task

--- FizzBuzz/Class1.cs
+++ FizzBuzz/Class1.cs
@@ -6,7 +6,48 @@ namespace FizzBuzz
     {
         public string CountOff(int num)
         {
-            return null;
+            var snum = num.ToString();
+            //string[] numlist = snum.Split();
+            bool b = snum.Contains("3");
+            if (b)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                if (num % 3 == 0 && num % 5 == 0 && num % 7 == 0)
+                {
+                    return "FizzBuzzWhizz";
+                }
+                else if (num % 3 == 0 && num % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                else if (num % 3 == 0 && num % 7 == 0)
+                {
+                    return "FizzWhizz";
+                }
+                else if (num % 5 == 0 && num % 7 == 0)
+                {
+                    return "BuzzWhizz";
+                }
+                else if (num % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else if (num % 5 == 0)
+                {
+                    return "Buzz";
+                }
+                else if (num % 7 == 0)
+                {
+                    return "Whizz";
+                }
+                else
+                {
+                    return snum;
+                }
+            }
         }
     }
 }
